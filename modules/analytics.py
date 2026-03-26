f241f4f2e4fe338d2ac383f3ecfe367b1e46319a
Ishan-Avishka <avishkaishan637@gmail.com>
2026-03-26 15:43:22 +0530

    Getting started with analytics file

---
+++ modules/analytics.py
@@ -0,0 +1,18 @@
+"""
+modules/analytics.py - Inventory Analytics Dashboard
+"""
+
+import tkinter as tk
+from tkinter import ttk
+from core.database import get_connection
+from core.theme import COLORS, FONTS
+from core.helpers import section_header, metric_card
+
+try:
+    import matplotlib
+    matplotlib.use("TkAgg")
+    from matplotlib.figure import Figure
+    from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+    MPL_OK = True
+except ImportError:
+    MPL_OK = False

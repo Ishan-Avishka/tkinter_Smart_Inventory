466b21ea786db32c390a8e593a4cd2b4f97c8ba3
Ishan-Avishka <avishkaishan637@gmail.com>
2026-03-29 18:46:32 +0530

    Getting started in products file

---
+++ modules/products.py
@@ -0,0 +1,19 @@
+"""
+modules/products.py - Product Management Module
+"""
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+from core.database import get_connection
+from core.theme import COLORS, FONTS
+from core.helpers import (make_scrollable_treeview, search_bar,
+                          section_header, confirm_dialog, info_dialog, error_dialog)
+
+
+class ProductsModule(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent, style="Panel.TFrame")
+        self.search_var = tk.StringVar()
+        self.search_var.trace_add("write", lambda *_: self._load_products())
+        self._build_ui()
+        self._load_products()

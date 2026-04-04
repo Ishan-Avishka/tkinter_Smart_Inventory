de1a302930ea06258a86231ac64d88b4133ef053
Ishan-Avishka <avishkaishan637@gmail.com>
2026-04-04 20:55:42 +0530

    Add config file to project and check everything runing perfectly

---
+++ config.py
@@ -0,0 +1,9 @@
+DB_CONFIG = {
+    "host":     "localhost",
+    "port":     3306,
+    "user":     "inv_user",
+    "password": "Ishan2003$$",
+    "database": "inventory_db",
+    "charset":  "utf8mb4",
+    "autocommit": False,
+}
